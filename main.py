3ddb15a757e19be0d27996298ea197f448a5028e
hanafication <rahardianardani@gmail.com>
2021-04-28 14:01:07 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from scraper.scraper import scraper
+
+scraper()
+# Press the green button in the gutter to run the script.
+#if __name__ == '__main__':
+#    scraper()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ scraper/scraper.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+
+def scraper():
+    '''' Inisialisasi webdriver
+    url = alamat yang dituju
+    '''
+    # Setting
+    chrome_options = webdriver.ChromeOptions()
+    #chrome_options.add_argument('--headless')
+    #chrome_options.add_argument('--no-sandbox')
+    #chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_experimental_option('detach', True)
+    driver = webdriver.Chrome('C:/Users/Rahadian/Documents/Python Scripts/chromedriver_win32/chromedriver.exe',
+                              chrome_options=chrome_options)
+
+    # Menuju URL
+    url = 'https://www.airbnb.com/'
+    driver = driver.get(url)
+    return driver
+
+
